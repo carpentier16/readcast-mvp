5047481d9bf4a7e9ffabb9f4ba2b74932e83985b
carpentier16 <adr.carpentier@gmail.com>
2025-08-11 21:42:14 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,11 @@
+// frontend/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
+import "./index.css"; // <— ceci doit exister exactement avec cette casse
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
