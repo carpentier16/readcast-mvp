e01e37dd38271c8b90ac0b27093e7e0432f27136
carpentier16 <adr.carpentier@gmail.com>
2025-08-13 01:10:45 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Configuration Vite avec JSX automatique
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 5173 },
-  build: { outDir: "dist" }
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+  ],
 });
