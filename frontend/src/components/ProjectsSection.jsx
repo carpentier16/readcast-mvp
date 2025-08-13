905ba17a1d76089ab0723249b1440d7415b14afa
carpentier16 <adr.carpentier@gmail.com>
2025-08-13 03:42:20 +0200

    Create ProjectsSection.jsx

---
+++ frontend/src/components/ProjectsSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProjectsSection = () => {
+  return (
+    <div className="projects-section">
+      <h2>Projects</h2>
+      <p>Your projects will appear here.</p>
+      {/* Ajoutez votre logique de projets ici */}
+    </div>
+  );
+};
+
+export default ProjectsSection;
