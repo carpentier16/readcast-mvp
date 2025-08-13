f7d63cc3c690d4428ded3a1e30e2bf69cdd99f27
carpentier16 <adr.carpentier@gmail.com>
2025-08-13 03:51:33 +0200

    Create index.js

---
+++ frontend/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './Header';
+export { default as UploadSection } from './UploadSection';
+export { default as VoiceSection } from './VoiceSection';
+export { default as ProjectsSection } from './ProjectsSection';
+export { default as ErrorBoundary } from './ErrorBoundary';
